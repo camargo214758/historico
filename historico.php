8ae6a7e1154bf153be2f45a0276bcc47382b69db
camargo214758 <guisafeju@gmail.com>
2022-06-22 16:56:06 -0300

    Initial commit

---
+++ historico.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$passwd = '';
+$bd_name = 'historico';
+
+ $connx = mysqli_connect($host, $user ,$passwd, $bd_name);
+
+ if($connx){
+ echo 'conectou';
+ }else{
+    echo 'erro';
+ }
+
+
+?>
